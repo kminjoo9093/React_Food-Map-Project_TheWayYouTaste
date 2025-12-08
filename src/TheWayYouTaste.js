58e43b4cb684362bc546ae8496031e53773594fe
minjoo <minjoook99@gmail.com>
2025-12-08 15:21:32 +0900

    search ui 초기구구성

--- src/TheWayYouTaste.js
+++ src/TheWayYouTaste.js
@@ -6,6 +6,7 @@ import Header from "./pages/Header";
 import Footer from "./pages/Footer";
 import ReportListCheck from "./pages/admin/ReportListCheck";
 import RegisterListCheck from "./pages/admin/RegisterListCheck";
+import SearchStore from "./pages/search/SearchStore";
 
 function TheWayYouTaste() {
   const location = useLocation();
@@ -22,6 +23,7 @@ function TheWayYouTaste() {
             <Route path="/admin/member/list" element={ <MemberListCheck /> }/> 
             <Route path="/admin/report/list" element={ <ReportListCheck /> }/> 
             <Route path="/admin/register/list" element={ <RegisterListCheck /> }/> 
+            <Route path="/search/store" element={ <SearchStore /> }/> 
             <Route path="/*" element={<Error404Page/>}/>
           </Routes>
           <Footer />
