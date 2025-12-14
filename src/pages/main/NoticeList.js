450b624b378c797fb06bbb1ce3d2dd89e6df7b2f
YunJung06 <a01056518116@gmail.com>
2025-12-15 08:51:56 +0900

    commit

--- src/pages/main/NoticeList.js
+++ src/pages/main/NoticeList.js
@@ -11,7 +11,7 @@ function Notice({ notices, isAdmin }) {
 
   const navigate = useNavigate();
   
-  const viewPeople = 1;
+  const viewPeople = 5;
   const lastIndex = nowPage * viewPeople;
   const firstIndex = lastIndex - viewPeople;
 
