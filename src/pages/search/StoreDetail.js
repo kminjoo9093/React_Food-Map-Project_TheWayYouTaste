f1161462ae9609d4fd467b01c24937833190264d
YunJung06 <a01056518116@gmail.com>
2025-12-22 11:46:08 +0900

    test

--- src/pages/search/StoreDetail.js
+++ src/pages/search/StoreDetail.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useSearchParams } from "react-router-dom";
 import starFill from "../../resources/img/search/iconStarFill.svg";
 import starHalf from "../../resources/img/search/iconStarHalf.svg";
 import ReviewRegister from "../../pages/review/ReviewRegister";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import styleStoreDetail from "../../css/StoreDetail.module.css";
+import { GetStoreList } from "./GetStoreList";
 
 const REVIEWS_PER_PAGE = 5;
 
