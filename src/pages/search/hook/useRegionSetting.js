c11f62e942fe40333558166f8db2a8a5a1d9ee93
minjoo <minjoook99@gmail.com>
2025-12-28 10:40:34 +0900

    storeDetail페이지 반응형, 메인 모바일 검색, 지도 기능 수정

--- src/pages/search/hook/useRegionSetting.js
+++ src/pages/search/hook/useRegionSetting.js
@@ -10,8 +10,6 @@ export default function useRegionSetting() {
     const [siName, setSiName] = useState("");
     const [selectedDong, setSelectedDong] = useState(null); //code
     const [dongName, setDongName] = useState("");
-    // const [isDimmedMiddleOpen, setIsDimmedMiddleOpen] = useState(false);
-    // const [isSelectedAll, setIsSelectedAll] = useState(false);
 
 
     const [lat, setLat] = useState(null); //37.5665
