edca30ce67317297acbd1403c64bbd808389d5e8
kminjoo9093 <kminjoo9093@naver.com>
2026-07-05 19:23:54 +0900

    refactor: storeApi TS 도입

--- src/types.ts
+++ src/types.ts
@@ -21,3 +21,30 @@ export type Dong = {
   dgCd: number;
   dgNm: string;
 };
+
+export type Viewport = {
+  [key: string]: number;
+};
+
+export type Store = {
+  bplcSn: number;
+  bplcNm: string;
+  address: string;
+  lat: number;
+  lng: number;
+  storeCatNo: string;
+  storeCatName: string;
+  avg: number;
+  dongCd: number;
+  sggCd: number;
+  sidoCd: number;
+  sggNm: string;
+  dongNm: string;
+  telno: string;
+  bgngTm: string;
+  ddlnTm: string;
+  parkingYn: boolean;
+  takeoutYn: boolean;
+  petYn: boolean;
+  bplcPhoto: string | null;
+};
